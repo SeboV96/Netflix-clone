d95144310c7ccd8fb450bf0b99348873a691156e
SeboV96 <sebovilla96@gmail.com>
2023-10-03 11:18:39 -0300

    Api calls almost complete

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import BackgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
@@ -6,10 +6,25 @@ import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
+import Slider from "../components/Slider";
 
 export default function Netflix() {
   const navigate = useNavigate()
   const [isScrolled, setIsScrolled] = useState(false);
+
+  const dispatch = useDispatch()
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const movies = useSelector((state) => state.netflix.movies)
+
+  useEffect(()=> {
+dispatch(getGenres())
+  },[])
+
+  useEffect(() => {
+    if (genresLoaded) dispatch(fetchMovies({type: "all"}))
+  },[genresLoaded])
 
   window.onscroll = () => {
     setIsScrolled(window.scrollY === 0 ? false : true);
@@ -39,6 +54,7 @@ export default function Netflix() {
           </div>
         </div>
       </div>
+      <Slider movies={movies}/>
     </Container>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "14be2e46ee86e19c6f680b2c8588a93d"
+export const TMBD_BASE_URL = "https://api.themoviedb.org/3"
